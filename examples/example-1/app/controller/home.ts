c4a2a40a3591f870e219d5a952ada228f7ba9083
topthinking <topthinking@ximalaya.com>
2020-01-15 13:14:12 +0800

    modify

--- examples/example-1/app/controller/home.ts
+++ examples/example-1/app/controller/home.ts
@@ -4,6 +4,7 @@ export default class HomeController extends Controller {
   public async index() {
     const { ctx } = this;
     console.log('model>>>', ctx.model.User);
+    console.log('model-sequelize>>>', ctx.model.User.find);
     ctx.body = 'hello world';
   }
 }
